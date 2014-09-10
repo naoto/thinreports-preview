0cb99d3738bf2eaa49ab7c15381dc9df25677ffd
Naoto SHINGAKI <shingaki@occ.co.jp>
2014-09-10 16:20:16 +0900

    add date format

--- lib/thinreports/preview/tblock.rb
+++ lib/thinreports/preview/tblock.rb
@@ -9,13 +9,55 @@ module Thinreports::Preview
       @config["id"].to_sym
     end
 
-    def preview_text(char = 'X')
-      char * word_lenght
+    def preview_text
+      if date?
+        format_type_char
+      else
+        ((format_type_char * word_lenght) + return_code) * line_length
+      end
     end
 
     private
+      def date?
+        @config["format"]["type"] == "datetime"
+      end
+
       def word_lenght
-        (@config["box"]["width"] / @config["svg"]["attrs"]["font-size"].to_i).ceil
+        length = (@config["box"]["width"] / @config["svg"]["attrs"]["font-size"].to_i)
+        if length < 1
+          1
+        else
+          length
+        end
+      end
+
+      def line_length
+        if @config["multiple"]
+          (@config["box"]["height"] / @config["svg"]["attrs"]["font-size"].to_i).ceil
+        else
+          1
+        end
+      end
+
+      def format_type_char
+        case @config["format"]["type"]
+        when "number"
+          "9"
+        when "padding"
+          "9"
+        when "datetime"
+          Time.now
+        else
+          "XX"
+        end
+      end
+
+      def return_code
+        if @config["multiple"]
+          "\n"
+        else
+          ""
+        end
       end
 
   end
